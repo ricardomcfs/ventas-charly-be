7f54308548a2dd79afe4a403e0d59c87d414e6b1
salvador MC <salvador.muncar@gmail.com>
2025-10-06 17:58:47 -0600

    se agrega nombre_tienda

--- src/controllers/categorias.controller.ts
+++ src/controllers/categorias.controller.ts
@@ -5,8 +5,6 @@ import { pool } from '../db';
 export const getCategorias = async (req: Request, res: Response) => {
   try {
     const [rows] = await pool.query("SELECT id, nombre FROM categorias ORDER BY id");
-    console.log('antes de......');
-    console.log(rows);
     res.json({ ok: true, categorias: rows });
   } catch (error: any) {
     res.status(500).json({ ok: false, error: error.message });
